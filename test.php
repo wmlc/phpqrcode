0846846bef62c20e1965bf41bffbc16403a4bf33
3wlog <1290800466@qq.com>
2015-11-29 00:28:12 +0800

    009

--- test.php
+++ test.php
@@ -1,2 +1,1 @@
-<?php //输出二维码图片123echo "<img alt='支付二维码' src='qrcode.php?data=<?php echo urlencode($url2);?>' />";?>
 <?php //输出二维码图片echo "<img alt='支付二维码' src='qrcode.php?data=<?php echo urlencode($url2);?>' />";?>
